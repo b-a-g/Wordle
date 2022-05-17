734774f618e26ccc62983390cf73132fa48c0c7e
aleksandr-belyaev <trustmeimanengineer42@gmail.com>
2022-05-17 21:47:49 +0700

    Удалил лишнее и поправил модель пользователя

--- Wordle/Models/UserViewModel.swift
+++ Wordle/Models/UserViewModel.swift
@@ -9,61 +9,62 @@ import SwiftUI
 import FirebaseAuth
 
 class UserViewModel: ObservableObject {
-  @AppStorage("isSignedIn") var isSignedIn = false
-  @Published var email = ""
-  @Published var password = ""
-  @Published var alert = false
-  @Published var alertMessage = ""
+    @AppStorage("isSignedIn") var isSignedIn = false
+    @Published var email = ""
+    @Published var password = ""
+    @Published var alert = false
+    @Published var alertMessage = ""
 
-  private func showAlertMessage(message: String) {
-    alertMessage = message
-    alert.toggle()
-  }
+    private func showAlertMessage(message: String) {
+        alertMessage = message
+        alert.toggle()
+    }
 
-  func login() {
-    // check if all fields are inputted correctly
-    if email.isEmpty || password.isEmpty {
-      showAlertMessage(message: "Neither email nor password can be empty.")
-      return
+    func login() {
+        // check if all fields are inputted correctly
+        if email.isEmpty || password.isEmpty {
+            showAlertMessage(message: "Neither email nor password can be empty.")
+            return
+        }
+        // sign in with email and password
+        Auth.auth().signIn(withEmail: email, password: password) { result, err in
+            if let err = err {
+                self.alertMessage = err.localizedDescription
+                self.alert.toggle()
+            } else if let result = result {
+                self.email = result.user.email ?? ""
+                self.isSignedIn = true
+            }
+        }
     }
-    // sign in with email and password
-    Auth.auth().signIn(withEmail: email, password: password) { result, err in
-      if let err = err {
-        self.alertMessage = err.localizedDescription
-        self.alert.toggle()
-      } else {
-        self.isSignedIn = true
-      }
-    }
-  }
 
-  func signUp() {
-    // check if all fields are inputted correctly
-    if email.isEmpty || password.isEmpty {
-      showAlertMessage(message: "Neither email nor password can be empty.")
-      return
+    func signUp() {
+        // check if all fields are inputted correctly
+        if email.isEmpty || password.isEmpty {
+            showAlertMessage(message: "Neither email nor password can be empty.")
+            return
+        }
+        // sign up with email and password
+        Auth.auth().createUser(withEmail: email, password: password) { result, err in
+            if let err = err {
+                self.alertMessage = err.localizedDescription
+                self.alert.toggle()
+            } else {
+                self.login()
+            }
+        }
     }
-    // sign up with email and password
-    Auth.auth().createUser(withEmail: email, password: password) { result, err in
-      if let err = err {
-        self.alertMessage = err.localizedDescription
-        self.alert.toggle()
-      } else {
-        self.login()
-      }
-    }
-  }
 
-  func logout() {
-    do {
-      try Auth.auth().signOut()
-      isSignedIn = false
-      email = ""
-      password = ""
-    } catch {
-      print("Error signing out.")
+    func logout() {
+        do {
+            try Auth.auth().signOut()
+            isSignedIn = false
+            email = ""
+            password = ""
+        } catch {
+            print("Error signing out.")
+        }
     }
-  }
 }
 
 let user = UserViewModel()
